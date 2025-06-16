1ed36ad1b2e097919ec26bd6ce6e7c9a9c152bb9
Galy Ricardo <the.programmer13.1986@gmail.com>
2025-06-15 21:38:04 -0500

    Cambio de ultimo momento
    Al probar al hacer inicio de sesión no estaba apareciendo el menu al loguearse, se agregó código adicional para resolver esto

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,14 +4,26 @@ import '../css/app.css'
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import AppLayout from './Layouts/AppLayout.vue'
+import GuestLayout from './Layouts/GuestLayout.vue'
 
 createInertiaApp({
   resolve: name => {
     const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
     const page = pages[`./Pages/${name}.vue`]
     
+    /*
     // Asignar el layout por defecto si no está definido
     if (page.default.layout === undefined) {
+      page.default.layout = AppLayout
+    }
+    else {
+      page.default.layout = GuestLayout
+    }
+      */
+    // Asignar layout según la ruta
+    if (name.startsWith('Auth/')) {
+      page.default.layout = GuestLayout
+    } else {
       page.default.layout = AppLayout
     }
     
